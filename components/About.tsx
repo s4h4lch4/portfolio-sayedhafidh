6cb8ea68cb96331ff3d00ac6fd756f3685087b10
s4h4lch4 <hafidhalkhaerid@gmail.com>
2025-10-22 10:33:04 +0700

    Update About.tsx
    Upload link cv

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About: React.FC<AboutProps> = ({ about }) => {
     <section>
        <h2 className="text-2xl font-bold text-slate-800 tracking-tight mb-4">About Me</h2>
        <p className="text-base leading-relaxed text-slate-600 mb-6">{about}</p>
-       <a href="/sayed-hafidh-cv.pdf" download className="inline-flex items-center gap-2 bg-teal-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-teal-600 transition-all duration-300 transform hover:scale-105">
+       <a href="https://drive.google.com/file/d/1FHWL_DwpJ647BovXgjC18Zok6R_T9he3/view?usp=sharing" download className="inline-flex items-center gap-2 bg-teal-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-teal-600 transition-all duration-300 transform hover:scale-105">
         <DownloadIcon className="h-5 w-5" />
         Download CV
        </a>
